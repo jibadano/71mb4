da775ea7e8ef0fd1eb6c28d1c0ba44d5776c3f48
Juan Ignacio Badano <jibadano@gmail.com>
2017-03-26 23:38:14 -0300

    asdasd

--- app/app.service.ts
+++ app/app.service.ts
@@ -13,8 +13,8 @@ export class AppService implements OnInit{
     timba : Timba = new Timba();
     timeCountDown : string;
     nav:string='welcome';
-    socket : any = io.connect('http://192.168.0.7:8081');
-    //socket : any = io.connect('http://186.22.78.117:8081');
+    //socket : any = io.connect('http://192.168.0.7:8081');
+    socket : any = io.connect('http://186.22.78.117:8081');
 
     exception : any;
 
