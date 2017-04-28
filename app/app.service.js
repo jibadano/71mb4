585a3532f2530f95d3ed137194d6fc7350fa89b0
Juan Ignacio Badano <jibadano@gmail.com>
2017-04-27 23:20:14 -0300

    aaaaaaaaaac

--- app/app.service.js
+++ app/app.service.js
@@ -24,7 +24,8 @@ var AppService = (function () {
         this.active = false;
         this.socketId = '';
         this.playing = true;
-        this.socket = io.connect('http://192.168.0.7:8081');
+        //socket : any = io.connect('http://192.168.0.7:8081');
+        this.socket = io.connect('http://186.22.76.215:8081');
         setInterval(function () {
             var playTime = new Date();
             playTime.setHours(16);
